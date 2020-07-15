b16ddc2ce56a668d3c31d947c4c9b4dda17885d9
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-07-15 15:22:28 -0400

    Better formating for generate.sh

--- generate.sh
+++ generate.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+#
+# Takes the content of /exports/data/ead/, runs them through the finding aid stylesheet
+# and places the resulting items in /var/www/html and /var/www/html/ead
+#
+
+
 EXPORTLOCATION=/exports/data/ead/
 EAD=$(find $EXPORTLOCATION -type f)
 WEBDIR=/var/www/html
@@ -8,7 +14,12 @@ for e in $EAD;
   do
   FILENAME=$(xmlstarlet sel -t -v "(//_:unitid)[1]" $e | sed 's/\./-/')
   echo "$DATE Generating html for $e" &>> /var/log/findingaid/findingaid.log
-  saxonb-xslt -o $WEBDIR/$FILENAME.html $e $STYLESHEET &>> /var/log/findingaid/findingaid.log && echo "Successful generation of $e" >> /var/log/findingaid/findingaid.log || echo "Generation for $e failed" >> /var/log/findingaid/findingaid.log
+  saxonb-xslt \
+      -o $WEBDIR/$FILENAME.html \
+      $e $STYLESHEET \
+      &>> /var/log/findingaid/findingaid.log \
+      && echo "Successful generation of $e" >> /var/log/findingaid/findingaid.log \
+	  || echo "Generation for $e failed" >> /var/log/findingaid/findingaid.log
   chown www-data:www-data $WEBDIR/$FILENAME.html
   cp $e $WEBDIR/ead/scihist-$FILENAME.xml
   chown www-data:www-data $WEBDIR/ead/scihist-$FILENAME.xml
