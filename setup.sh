9a2bf9188711a1a391fa575ce58299e508913569
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-07-15 15:22:08 -0400

    Adding a log file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
-pip install requests
-pip install psutil
-pip install requests_toolbelt
-sudo mkdir -p /exports/data/ead
+# These commands can be removed from here and
+# added to the Ansible script for aspace.
+#
+pip install requests  psutil requests_toolbelt
+
+sudo mkdir -p /exports/data/ead  /var/www/html/ead/ /var/log/findingaid/
+sudo chown -R ubuntu:ubuntu /exports/data/ead
+sudo chown -R ubuntu:ubuntu /var/www/html/ead/
+sudo chown -R ubuntu:ubuntu /var/log/findingaid/
+
+sudo touch /var/log/findingaid/findingaid.log
+sudo chown ubuntu:ubuntu /var/log/findingaid/findingaid.log
