b41d5b2776a50ca3700fca2324417ce8c8c6ee03
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-07-21 11:00:59 -0400

    Better checks: ensure that ubuntu is allowed to write to www-data, and fix the archivesspace check.

--- complete_export.sh
+++ complete_export.sh
@@ -1,9 +1,17 @@
-echo "Checking that ArchivesSpace is running"
+echo "Checking that ArchivesSpace is running..."
 if [[ $(service archivesspace status | grep '\sactive') ]]; then
     echo "ArchivesSpace is running."
 else
     echo "ArchivesSpace is not running."
     echo "Please run sudo systemctl start archivesspace until it is running."
+    exit
+fi
+
+echo "Checking that ubuntu is part of group www-data..."
+if [[ $( groups | grep www-data ) ]]; then
+    echo "ubuntu is part of www-data."
+else
+    echo "User ubuntu is not part of group www-data. This script will not be able to write to the web directories."
     exit
 fi
 
