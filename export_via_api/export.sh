130a500a6ead8b023c03e9993400459f5729daa0
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-10-12 13:19:21 -0400

    URL should be its own variable

--- export_via_api/export.sh
+++ export_via_api/export.sh
@@ -1,8 +1,11 @@
 source ../script_settings.sh
 OUTPUT_DIR=eads
-SESSION=`curl -s  -F password="$EXPORT_PASSWORD" archives.sciencehistory.org:8089/users/$EXPORT_USER/login |  jq -r '.session'`
-IDS=`curl -s -H "X-ArchivesSpace-Session: $SESSION" archives.sciencehistory.org:8089/repositories/$REPOSITORY_ID/resources?all_ids=true |  jq -c '.[] ' `
+API_URL=archives.sciencehistory.org:8089
+SESSION=`curl -s  -F password="$EXPORT_PASSWORD" $API_URL/users/$EXPORT_USER/login |  jq -r '.session'`
+IDS=`curl -s -H "X-ArchivesSpace-Session: $SESSION" $API_URL/repositories/$REPOSITORY_ID/resources?all_ids=true |  jq -c '.[] ' `
 for ID in $IDS; do
 	echo "Exporting $ID."
-	curl -s -H "X-ArchivesSpace-Session: $SESSION" archives.sciencehistory.org:8089/repositories/$REPOSITORY_ID/resource_descriptions/$ID.xml > $OUTPUT_DIR/$ID.ead
+	curl -s -H "X-ArchivesSpace-Session: $SESSION" \
+		$API_URL/repositories/$REPOSITORY_ID/resource_descriptions/$ID.xml \
+		> $OUTPUT_DIR/$ID.ead
 done
