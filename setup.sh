d0b07f4585b1d9b5ff805ebea648025446351461
Eddie Rubeiz <erubeiz@sciencehistory.org>
2021-12-08 15:52:47 -0500

    Export directly to /var/www/html/new_ead HTML transform goes from /var/www/html/new_ead to /var/www/html/ Remove more python requirements (pip install, settings file backup)

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 # These commands can be removed from here and
 # added to the Ansible script for aspace.
-#
-pip install requests  psutil requests_toolbelt
+sudo mkdir -p  /var/www/html/ead/ /var/www/html/new_ead/ /var/log/findingaid/
 
-sudo mkdir -p /exports/data/ead  /var/www/html/ead/ /var/log/findingaid/
-sudo chown -R ubuntu:ubuntu /exports/data/ead
 sudo chown -R ubuntu:ubuntu /var/log/findingaid/
 
 sudo touch /var/log/findingaid/findingaid.log
@@ -12,5 +9,6 @@ sudo chown ubuntu:ubuntu /var/log/findingaid/findingaid.log
 
 sudo chown -R www-data:www-data /var/www/
 sudo chmod -R g+rwx /var/www
+
 # Note: It's assumed ubuntu is part of group wwww-data and can thus write to these directories.
 # If it's not, the export script will simply not run.
